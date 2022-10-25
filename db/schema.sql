94e03977fbeca2023ad7798cfd4e937e001a16c5
swyner97 <swyner97@gmail.com>
2022-10-25 16:29:21 -0700

    working on inquirer functions

--- db/schema.sql
+++ db/schema.sql
@@ -21,5 +21,5 @@ CREATE TABLE employee (
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT NOT NULL
-)
+    manager_id INT; 
+);
--- index.js
+++ index.js
@@ -1,50 +1,65 @@
 import inquirer from 'inquirer'
+import mysql from 'mysql2'
+import cTable from 'console.table'
 import { of } from 'rxjs'
 
-
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'employees'
+})
 // THEN I am presented with the following options: View all departments, View all roles, View all employees, Add a department, Add a role, Add an employee, and update an employee role
 
 
-let appMenu = () => {
-    inquirer.prompt([
+let appMenu =[
         {
             type: "list",
             name: "initial",
             message: "What would you like to do?",
             choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
         }
-    ])
-}
+    ]
+
+
 
 let init = () => {
     inquirer.prompt(appMenu)
         .then((answer) => {
-            const { choices } = answer;
-            if (choices === "View all departments") {
-                viewAllDepartments();
+            if (answer === "View all departments") {
+                viewDepartments();
             }
-            if (choices === " View all rolls") {
+            if (answer === " View all rolls") {
                 viewAllRolls()
             }
-            if (choices === "View all employees") {
-                viewAllEmployees()
+            if (answer === "View all employees") {
+                viewEmployees()
             }
-            if (choices === "Add a department") {
-                addADepartment()
+            if (answer === "Add a department") {
+                addDepartment()
             }
-            if (choices === "Add a role") {
-                addARole()
+            if (answer === "Add a role") {
+                addRole()
             }
-            if (choices === "Add an employee") {
-                addAnEmployee()
+            if (answer === "Add an employee") {
+                addEmployee()
             }
-            if (choices === "Update an employee role") {
+            if (answer === "Update an employee role") {
                 updateEmpRole()
             }
         })
 }
 
 
-let viewAllDepartments = () => {
-    
+let viewDepartments = () => {
+    connection.query(
+        `SELECT * FROM department`,
+        function (err, results, fields) {
+            console.table(results);
+            console.log(fields)
+        }
+    )
+    init()
+
 }
+
+init()
